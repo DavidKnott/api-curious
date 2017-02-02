b7009bf23e5aa9740504e716a8d7b0dbe66c5c47
David Knott <david1k1nott@gmail.com>
2017-02-02 13:05:56 -0700

    add refresh functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,21 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    byebug
     @current_user ||= User.find(session[:user_id])
   end
 
   def check_for_refresh
-    if current_token
+    conn = Faraday.new
+    conn.headers['Authorization'] = "bearer #{current_user.token}"
+    response = conn.get "https://oauth.reddit.com/api/v1/me"
+    if response.status == 401
+      conn = Faraday.new 
+      conn.basic_auth(ENV["REDDIT_KEY"], ENV["REDDIT_SECRET"])
+      conn.params = {"grant_type" => "refresh_token", "refresh_token" => "#{current_user.refresh_token}"}
+      response = conn.post "https://www.reddit.com/api/v1/access_token"
+      response = JSON.parse(response.body)
+      current_user.refresh(response.body)
+    end
   end
 
 
